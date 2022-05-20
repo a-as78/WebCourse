edeb828ac5b403750004a05d37eeaa297d26493c
Anahita Asghari <anahita.as78@gmail.com>
2022-05-20 18:49:03 +0430

    adding header and post preview component

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/header'
+import PostPreview from './components/homepage/postPreview'
 function App() {
+  const name = 'Ana';
+  const welcomeText = 'Welcome to My Project!';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header welcomeText={welcomeText} />
+        <PostPreview authorTitle={name} />
     </div>
   );
 }
